79f8839d7af34bc31cf5721c4ec8b61368bf364f
Emilio <tryskull2005@gmail.com>
2023-11-17 22:17:49 -0600

    Agregar nuevos archivos

---
+++ P2.P93.py
@@ -0,0 +1,10 @@
+import os
+
+archivo = "colores.txt"
+
+if os.path.exists(archivo):
+    print("\nEl archivo ya existe")
+else:
+    print("\nEl archivo no existe")
+
+
---
+++ P4.94.py
@@ -0,0 +1,13 @@
+import os
+
+archivo = "colores.txt"
+
+with open(archivo, "a+") as f:
+    # El archivo se abre automáticamente en el bloque 'with'
+    pass  # Puedes realizar operaciones adicionales dentro de este bloque si es necesario
+
+# Verificación de que ya se creó.
+if os.path.exists(archivo):
+    print("\nEl archivo ya existe")
+else:
+    print("\nEl archivo no existe")
